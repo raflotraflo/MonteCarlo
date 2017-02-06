c34111d5cce3226052a1a8bfdfe64571646b22be
Rafał Chodzidło <rchodzidlo@aiut.com.pl>
2017-02-06 16:28:00 +0100

    Add project files.

---
+++ MonteCarlo/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace MonteCarlo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBoxThreadCount = new System.Windows.Forms.ComboBox();
+            this.ButtonCalculate = new System.Windows.Forms.Button();
+            this.textBoxTime = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // comboBoxThreadCount
+            // 
+            this.comboBoxThreadCount.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxThreadCount.Items.AddRange(new object[] {
+            1,
+            2,
+            3,
+            4});
+            this.comboBoxThreadCount.Location = new System.Drawing.Point(12, 43);
+            this.comboBoxThreadCount.Name = "comboBoxThreadCount";
+            this.comboBoxThreadCount.Size = new System.Drawing.Size(158, 24);
+            this.comboBoxThreadCount.TabIndex = 0;
+            // 
+            // ButtonCalculate
+            // 
+            this.ButtonCalculate.Location = new System.Drawing.Point(362, 43);
+            this.ButtonCalculate.Name = "ButtonCalculate";
+            this.ButtonCalculate.Size = new System.Drawing.Size(132, 45);
+            this.ButtonCalculate.TabIndex = 1;
+            this.ButtonCalculate.Text = "Oblicz";
+            this.ButtonCalculate.UseVisualStyleBackColor = true;
+            this.ButtonCalculate.Click += new System.EventHandler(this.buttonCalculate_Click);
+            // 
+            // textBoxTime
+            // 
+            this.textBoxTime.Location = new System.Drawing.Point(12, 213);
+            this.textBoxTime.Name = "textBoxTime";
+            this.textBoxTime.ReadOnly = true;
+            this.textBoxTime.Size = new System.Drawing.Size(158, 22);
+            this.textBoxTime.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Ilość wątków";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 193);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(95, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Czas obliczeń";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(532, 448);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxTime);
+            this.Controls.Add(this.ButtonCalculate);
+            this.Controls.Add(this.comboBoxThreadCount);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button ButtonCalculate;
+        private System.Windows.Forms.TextBox textBoxTime;
+        private System.Windows.Forms.ComboBox comboBoxThreadCount;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ MonteCarlo/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MonteCarlo
+{
+    public partial class Form1 : Form
+    {
+        private int i = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonCalculate_Click(object sender, EventArgs e)
+        {
+            this.textBoxTime.Text = "test  = " + i;
+            i++;
+        }
+    }
+}
